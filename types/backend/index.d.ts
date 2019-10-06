27b422150885515298101bd57d6883691ab59963
Kobi Nemni <kobin@wix.com>
2019-10-06 17:25:56 +0300

    fix(types files): add buffer and missing $w declaration parts

--- types/backend/index.d.ts
+++ types/backend/index.d.ts
@@ -1,1 +1,8 @@
 /// <reference path="../common/declaration.d.ts" />
+
+// declaration file is not splitted into atomic pieces
+// some of the apis are using $w.$w type
+// in order to pass compilation we add mock $w.$w declaration
+declare namespace $w {
+    type $w = any;
+}
--- types/pages/index.d.ts
+++ types/pages/index.d.ts
@@ -1,2 +1,8 @@
+/// <reference path="../common/$w.d.ts" />
 /// <reference path="../common/declaration.d.ts" />
-/// <reference path="./$w.d.ts" />
+
+// declaration file is not splitted into atomic pieces
+// WixHttpFunctionResponse and mediaManager is using Buffer type
+// in order to pass compilation we add mock Buffer class
+declare class Buffer {
+}
