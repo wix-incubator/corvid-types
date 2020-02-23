52eb480b8bac4578818ca086d21784076938d77f
Kobi Nemni <kobin@wix.com>
2020-02-23 13:12:44 +0200

    0.1.95

--- scripts/generate-full-corvid-types.js
+++ scripts/generate-full-corvid-types.js
@@ -5,17 +5,42 @@ const path = require("path");
 const ts = require("typescript");
 const without_ = require("lodash/without");
 
+// process.env.RUN_BUILD_IN_ROOT_FOLDER = true;
+
 const { TS_CONFIG_PATHS, TS_CONFIG_BASE_PATH } = require("../src/constants");
 const FULL_CORVID_DECLARATION_NAME = "fullCorvidTypes.json";
 
+// let i = 0;
+
 const prepareEmptyTypescriptProgram = configPath => {
   const tmpDirPath = tmp.dirSync().name;
+  const corvidDir = path.join(__dirname, "../");
+  const corvidTsConfigPath = path.join(corvidDir, configPath);
+  // create .tmp folder
+  // const tmpDirPath = path.resolve(__dirname, "../", `emptyTsProject${i}`);
+  // fs.ensureDirSync(tmpDirPath);
+  // i++;
 
   const tmpConfigPath = `${tmpDirPath}/tsconfig.json`;
-  const tmpConfigContent = `{"extends": "${path.relative(
-    tmpConfigPath,
+  const tmpConfigContent = `{"extends": "${path.resolve(
+    corvidDir,
     configPath
   )}"}`;
+
+  // const tmpConfigContent = `{"extends": "../${configPath}"}`;
+
+  console.log("tmpDirPath", tmpDirPath);
+  console.log("corvidDir", corvidDir);
+  console.log("corvidTsConfigPath", corvidTsConfigPath);
+  // console.log(fs.readdirSync(corvidDir));
+  // console.log("tmpDirPath2", tmpDirPath2);
+
+  console.log("tmpConfigPath", tmpConfigPath);
+  console.log("tmpConfigContent", tmpConfigContent);
+  console.log("configPath", configPath);
+  console.log("configPath", path.join(__dirname, "../", configPath));
+  console.log("path", path.relative(tmpConfigPath, configPath));
+  console.log("*******************************************");
 
   fs.writeFileSync(tmpConfigPath, tmpConfigContent);
   fs.writeFileSync(`${tmpDirPath}/empty.js`, "");
@@ -46,6 +71,8 @@ const getDeclarationFilesFromTsConfig = configPath => {
 };
 
 async function generateFullCorvidDeclarations() {
+  console.log("LOCAL_DOCS_REPO_PATH", process.env.LOCAL_DOCS_REPO_PATH);
+  console.log("__dirname", __dirname);
   const corvidLib = {
     BASE: getDeclarationFilesFromTsConfig(TS_CONFIG_BASE_PATH)
   };
