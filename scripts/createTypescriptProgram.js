ded90345967a016d3a64550bfa3aafe133414ff4
matanlb <matanli@wix.com>
2020-06-22 13:58:01 +0300

    test(get-wix-module-list): move error check to create ts program

--- scripts/createTypescriptProgram.js
+++ scripts/createTypescriptProgram.js
@@ -1,8 +1,30 @@
 const tmp = require("tmp");
 const ts = require("typescript");
 const fs = require("fs-extra");
+const _ = require("lodash");
 
 tmp.setGracefulCleanup(); // cleans tmp file on process exit
+
+/**
+ * Get a typescript compiled program and if has compile time error will throw error with
+ * a formalized errors breakdown
+ * @param {typescript.Program} program
+ */
+function detectErrors(program) {
+  const syntaxErrors = program
+    .getSyntacticDiagnostics()
+    .filter(diagnostic => diagnostic.category === ts.DiagnosticCategory.Error);
+  if (_.isEmpty(syntaxErrors)) return;
+
+  const errors = syntaxErrors.map(
+    ({ file, start, messageText, code }) =>
+      `\tAt ${file.path} position ${start} - ${messageText} (code - ${code})`
+  );
+  throw new Error(
+    "Encountered the following error(s) while compiling:\n" +
+      _.join(errors, "\n")
+  );
+}
 
 const prepareEmptyTypescriptProgram = configPath => {
   const tmpDir = tmp.dirSync();
@@ -29,8 +51,13 @@ module.exports = function createTsProgram(tsConfigPath) {
 
   const { options, fileNames } = parsedCmd;
 
-  return ts.createProgram({
+  const program = ts.createProgram({
     rootNames: fileNames,
     options
   });
+
+  // Check for compiler errors and throw if so
+  detectErrors(program);
+
+  return program;
 };
--- scripts/extractDtsModules.js
+++ scripts/extractDtsModules.js
@@ -1,32 +1,10 @@
 const tmp = require("tmp");
 const fs = require("fs-extra");
 const _ = require("lodash");
-const { DiagnosticCategory } = require("typescript");
 
 const createTsProgram = require("./createTypescriptProgram");
 
 tmp.setGracefulCleanup(); // cleans tmp file on process exit
-
-/**
- * Get a typescript compiled program and if has compile time error will throw error with
- * a formalized errors breakdown
- * @param {typescript.Program} program
- */
-function detectErrors(program) {
-  const syntaxErrors = program
-    .getSyntacticDiagnostics()
-    .filter(diagnostic => diagnostic.category === DiagnosticCategory.Error);
-  if (_.isEmpty(syntaxErrors)) return;
-
-  const errors = syntaxErrors.map(
-    ({ file, start, messageText, code }) =>
-      `\tAt ${file.path} position ${start} - ${messageText} (code - ${code})`
-  );
-  throw new Error(
-    "Encountered the following error(s) while compiling:\n" +
-      _.join(errors, "\n")
-  );
-}
 
 /**
  * Takes in an array of dts files paths and returns a list of modules declared
@@ -45,9 +23,6 @@ module.exports = function extractModules(dtsPaths = []) {
   const configFile = tmp.fileSync();
   fs.writeFileSync(configFile.fd, JSON.stringify(tsConfig));
   const program = createTsProgram(configFile.name);
-
-  // Throws in case of compiler time error
-  detectErrors(program);
 
   return _.chain(program)
     .invoke("getSourceFiles")
