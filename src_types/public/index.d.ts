f12b227798ffc40c8d5b12ad92ab59dbe3c33e2a
Matan Lieberman <matanli@wix.com>
2020-08-04 17:51:28 +0300

    feat(json files): allow importing json files (#38)

--- src_types/public/index.d.ts
+++ src_types/public/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="../common/emptyBuffer.d.ts" />
 /// <reference path="../common/void$w$w.d.ts" />
 /// <reference path="../common/declaration.d.ts" />
+/// <reference path="../common/jsonFiles.d.ts" />
