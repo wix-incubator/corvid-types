149ebdd8d4176cb9b7e7cc407567249e6635f343
Matan Lieberman <matanli@wix.com>
2020-09-02 17:43:31 +0300

    feat(json-files): re-allow json files (#42)
    This reverts commit ba04169ec1ee73cf7d398480731caea8dc86980b.

--- src_types/pages/index.d.ts
+++ src_types/pages/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="../common/emptyBuffer.d.ts" />
 /// <reference path="./$w.d.ts" />
 /// <reference path="../common/declaration.d.ts" />
+/// <reference path="../common/jsonFiles.d.ts" />
