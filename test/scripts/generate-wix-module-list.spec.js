50d2b1a25c94ba77c24d09af85cd85a8ad70f106
Dor Itzhaki <dori@wix.com>
2020-09-13 23:47:13 +0300

    refactor: move all build output to the dist folder

--- test/scripts/generate-wix-module-list.spec.js
+++ test/scripts/generate-wix-module-list.spec.js
@@ -1,6 +1,6 @@
 describe("generate wix module list", () => {
   it("Check output json", () => {
-    const modules = require("../../wixModules.json");
+    const modules = require("../../dist/wixModules.json");
     modules.forEach(module => expect(module).toMatch(/^wix-/));
   });
 });
