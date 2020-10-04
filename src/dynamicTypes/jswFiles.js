34a36b847195a78fac670d337def29b063c84457
Kobi Nemni <kobin@wix.com>
2020-10-04 13:44:48 +0300

    fix: enable all backend imports

--- src/dynamicTypes/jswFiles.js
+++ src/dynamicTypes/jswFiles.js
@@ -1,7 +1,7 @@
 // TODO: Get the project file tree as a paramter and generate a d.ts containing a deceleration
 //       foreach .jsw file (i.e remove the wildcard)
 
-const getJswRawDeclaration = () => "declare module 'backend/*.jsw';";
+const getJswRawDeclaration = () => "declare module 'backend/*';";
 
 function getJswTypeDeclarations() {
   return [
