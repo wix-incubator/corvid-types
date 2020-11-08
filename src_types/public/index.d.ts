b09a63701b952d079b7349581bae2557851be58d
Kobi Nemni <kobin@wix.com>
2020-11-08 16:58:34 +0200

    feat(public context): allow any  string selector in public files

--- src_types/public/index.d.ts
+++ src_types/public/index.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="../common/anyProperties.d.ts" />
 /// <reference path="../common/emptyBuffer.d.ts" />
-/// <reference path="../common/void$w$w.d.ts" />
 /// <reference path="../common/declaration.d.ts" />
+/// <reference path="./$w.d.ts" />
