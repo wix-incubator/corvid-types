c0960e174023d656cf7f3ca0d7e0fde1c9e6ef82
yaron-miron <yaronm@wix.com>
2021-12-21 13:46:47 +0200

    make ast more readable

--- scripts/ast-patches/changeWixAuthElevateFunctionType.js
+++ scripts/ast-patches/changeWixAuthElevateFunctionType.js
@@ -20,83 +20,24 @@ function changeWixAuthElevateFunctionType(ast) {
     return ast;
   }
 
-  // ast code was added with the help of https://ts-ast-viewer.com/#
-  elevateFunction.typeParameters = [getFunctionTypeParameter()];
-  elevateFunction.parameters = [getFunctionParameter()];
-  elevateFunction.type = getFunctionReturnType();
+  elevateFunction.typeParameters = [
+    ts.factory.createTypeParameterDeclaration("T extends (...arg: any) => any")
+  ];
+
+  elevateFunction.parameters = [
+    ts.factory.createParameterDeclaration(
+      undefined,
+      undefined,
+      undefined,
+      "func: T"
+    )
+  ];
+
+  elevateFunction.type = ts.factory.createTypeReferenceNode(
+    "(...param: Parameters<T>) => ReturnType<T>"
+  );
 
   return ast;
-}
-
-function getFunctionReturnType() {
-  return ts.factory.createFunctionTypeNode(
-    undefined,
-    [
-      ts.factory.createParameterDeclaration(
-        undefined,
-        undefined,
-        ts.factory.createToken(ts.SyntaxKind.DotDotDotToken),
-        ts.factory.createIdentifier("param"),
-        undefined,
-        ts.factory.createTypeReferenceNode(
-          ts.factory.createIdentifier("Parameters"),
-          [
-            ts.factory.createTypeReferenceNode(
-              ts.factory.createIdentifier("T"),
-              undefined
-            )
-          ]
-        ),
-        undefined
-      )
-    ],
-    ts.factory.createTypeReferenceNode(
-      ts.factory.createIdentifier("ReturnType"),
-      [
-        ts.factory.createTypeReferenceNode(
-          ts.factory.createIdentifier("T"),
-          undefined
-        )
-      ]
-    )
-  );
-}
-
-function getFunctionParameter() {
-  return ts.factory.createParameterDeclaration(
-    undefined,
-    undefined,
-    undefined,
-    ts.factory.createIdentifier("func"),
-    undefined,
-    ts.factory.createTypeReferenceNode(
-      ts.factory.createIdentifier("T"),
-      undefined
-    ),
-    undefined
-  );
-}
-
-function getFunctionTypeParameter() {
-  return ts.factory.createTypeParameterDeclaration(
-    ts.factory.createIdentifier("T"),
-    ts.factory.createFunctionTypeNode(
-      undefined,
-      [
-        ts.factory.createParameterDeclaration(
-          undefined,
-          undefined,
-          ts.factory.createToken(ts.SyntaxKind.DotDotDotToken),
-          ts.factory.createIdentifier("arg"),
-          undefined,
-          ts.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
-          undefined
-        )
-      ],
-      ts.factory.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword)
-    ),
-    undefined
-  );
 }
 
 module.exports = changeWixAuthElevateFunctionType;
