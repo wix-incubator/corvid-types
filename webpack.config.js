6006c387fe310b247897090172c8d69724fa502d
Dolev Hadar <dolevh@wix.com>
2022-01-05 17:52:55 +0200

    bring back umd

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,19 @@ const path = require("path");
 const env = process.env.NODE_ENV || "development";
 const DIST = path.join(__dirname, "./dist");
 
-module.exports = {
+const baseConfig = libraryTarget => ({
   mode: env === "development" ? "development" : "production",
   entry: "./src/index.ts",
   experiments: {
-    outputModule: true
+    outputModule: libraryTarget === "esm"
   },
   output: {
-    filename: `corvidTypes.esm.js`,
-    chunkFilename: "[name].chunk.js",
+    filename: `corvidTypes.${libraryTarget}.js`,
+    chunkFilename: `[name].${libraryTarget}.chunk.js`,
     path: DIST,
     library: {
-      type: "module"
+      ...(libraryTarget === "esm" ? {} : { name: "corvidTypes" }),
+      type: libraryTarget === "esm" ? "module" : "var"
     }
   },
   resolve: {
@@ -38,4 +39,6 @@ module.exports = {
       }
     ]
   }
-};
+});
+
+module.exports = [baseConfig("esm"), baseConfig("umd")];
