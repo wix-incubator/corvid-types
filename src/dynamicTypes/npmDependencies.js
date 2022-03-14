0b8613f2bc7ff970809b76e864caa72868dd0d17
Yury Michurin <yurym@wix.com>
2022-03-14 16:20:55 +0200

    Add timeout for server request

--- src/dynamicTypes/npmDependencies.js
+++ src/dynamicTypes/npmDependencies.js
@@ -4,28 +4,38 @@ const getDefaultPackageRawDeclaration = name =>
     declare module '${name}/*';
   `;
 
-const BASE_PACKAGE_TYPES_URL = `https://unpkg.com`;
+const BASE_PACKAGE_TYPES_URL = `https://bo.wix.com/_serverless/package-types-resolver/`;
+const SERVER_TIMEOUT_MS = 5 * 1000;
 
-async function getRemoteTypeDeclaration(packageName) {
+async function getRemoteTypeDeclarationFromServer(packageName, packageVersion) {
+  // eslint-disable-next-line no-undef
+  const typeDeclarationResponse = await fetch(
+    `${BASE_PACKAGE_TYPES_URL}/${packageName}@${packageVersion}`
+  );
+
+  if (!typeDeclarationResponse.ok) {
+    return getDefaultPackageRawDeclaration(packageName);
+  }
+
+  return typeDeclarationResponse.text();
+}
+
+async function getRemoteTypeDeclaration(packageName, packageVersion) {
   try {
-    // eslint-disable-next-line no-undef
-    const typeDeclarationResponse = await fetch(
-      `${BASE_PACKAGE_TYPES_URL}/${packageName}/velo-types/index.d.ts`
-    );
+    const result = await Promise.race([
+      new Promise((_, reject) => setTimeout(reject, SERVER_TIMEOUT_MS)),
+      getRemoteTypeDeclarationFromServer(packageName, packageVersion)
+    ]);
 
-    if (!typeDeclarationResponse.ok) {
-      return getDefaultPackageRawDeclaration(packageName);
-    }
-
-    return typeDeclarationResponse.text();
+    return result;
   } catch (ex) {
     return getDefaultPackageRawDeclaration(packageName);
   }
 }
 
-async function getPackageRawDeclaration(packageName) {
+async function getPackageRawDeclaration(packageName, packageVersion) {
   if (packageName.startsWith("@wix/")) {
-    return getRemoteTypeDeclaration(packageName);
+    return getRemoteTypeDeclaration(packageName, packageVersion);
   }
 
   return getDefaultPackageRawDeclaration(packageName);
@@ -40,7 +50,10 @@ async function getPackagesTypeDeclarations(dependencies = {}) {
   const npmModulesTypeFilesPromises = dependenciesNames.map(
     async packageName => ({
       path: `/dependencies/${packageName}.${dependencies[packageName]}.d.ts`,
-      content: await getPackageRawDeclaration(packageName)
+      content: await getPackageRawDeclaration(
+        packageName,
+        dependencies[packageName]
+      )
     })
   );
 
